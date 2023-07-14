c5974729e3ee818c4212238e017760c6b9f8a29c
vandoris <vandoris@gmail.com>
2023-07-14 16:51:19 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/vandoris/springbootmall/dao/ProductDao.java
+++ src/main/java/com/vandoris/springbootmall/dao/ProductDao.java
@@ -1,7 +1,10 @@
 package com.vandoris.springbootmall.dao;
 
+import com.vandoris.springbootmall.dto.ProductRequest;
 import com.vandoris.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/vandoris/springbootmall/service/ProductService.java
+++ src/main/java/com/vandoris/springbootmall/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.vandoris.springbootmall.service;
 
+import com.vandoris.springbootmall.dto.ProductRequest;
 import com.vandoris.springbootmall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
