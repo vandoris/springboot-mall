12dcc8be4cb4c3c1bd64500b96a286742345037e
vandoris <vandoris@gmail.com>
2023-07-17 15:41:35 +0800

    商品功能 - 實作查詢商品列表（分頁）

--- src/main/java/com/vandoris/springbootmall/controller/ProductController.java
+++ src/main/java/com/vandoris/springbootmall/controller/ProductController.java
@@ -10,12 +10,16 @@ import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.Calendar;
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
 
@@ -30,13 +34,19 @@ public class ProductController {
 
             // 排序條件 sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+
+            // 分頁 pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
     ) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
