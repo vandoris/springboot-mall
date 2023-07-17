d049dea81e52f6538fdff5ce6a9f521ffa9b3f99
vandoris <vandoris@gmail.com>
2023-07-17 10:48:57 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/vandoris/springbootmall/controller/ProductController.java
+++ src/main/java/com/vandoris/springbootmall/controller/ProductController.java
@@ -59,4 +59,10 @@ public class ProductController {
     }
 
 
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
