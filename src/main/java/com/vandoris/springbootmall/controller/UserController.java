7f8282740485d3e50e22264b42c5b69f583de9b6
vandoris <vandoris@gmail.com>
2023-07-18 14:26:35 +0800

    帳號功能 - 登入

--- src/main/java/com/vandoris/springbootmall/controller/UserController.java
+++ src/main/java/com/vandoris/springbootmall/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.vandoris.springbootmall.controller;
 
+import com.vandoris.springbootmall.dto.UserLoginRequest;
 import com.vandoris.springbootmall.dto.UserRegisterRequest;
 import com.vandoris.springbootmall.model.User;
 import com.vandoris.springbootmall.service.UserService;
@@ -25,5 +26,12 @@ public class UserController {
         User user = userService.getUserById(userId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
--- src/main/java/com/vandoris/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/vandoris/springbootmall/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.vandoris.springbootmall.service.impl;
 
 import com.vandoris.springbootmall.dao.UserDao;
+import com.vandoris.springbootmall.dto.UserLoginRequest;
 import com.vandoris.springbootmall.dto.UserRegisterRequest;
 import com.vandoris.springbootmall.model.User;
 import com.vandoris.springbootmall.service.UserService;
@@ -36,5 +37,22 @@ public class UserServiceImpl implements UserService {
 
         // 創建帳號
         return userDao.createUser(userRegisterRequest);
+    }
+
+    @Override
+    public User login(UserLoginRequest userLoginRequest) {
+        User user = userDao.getUserByEmail(userLoginRequest.getEmail());
+
+        if (user == null) {
+            log.warn("該 email {} 尚未註冊" + userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+            return user;
+        } else {
+            log.warn("email {} 的密碼不正確" + userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
