2218820ea54e3894063579b720f633fe0de072b9
Vandoris <vandoris@gmail.com>
2025-09-23 17:35:34 +0800

    商品功能 - 實作查詢商品列表(分頁)

--- src/main/java/com/vandoris/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/vandoris/springbootmall/dao/impl/ProductDaoImpl.java
@@ -32,6 +32,7 @@ public class ProductDaoImpl implements ProductDao {
 
         Map<String, Object> map = new HashMap<>();
 
+        // 查詢條件
         if (productQueryParams.getCategory() != null) {
             sql += " AND category = :category";
             map.put("category", productQueryParams.getCategory().name());
@@ -42,7 +43,14 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
 
+        // 排序
         sql += " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+        // 分頁
+        sql += " LIMIT " + productQueryParams.getLimit() + " OFFSET " + productQueryParams.getOffset();
+//        sql += " LIMIT :limit OFFSET :offset";
+//        map.put("limit", productQueryParams.getLimit());
+//        map.put("offset", productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
