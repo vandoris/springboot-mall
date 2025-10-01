486ab82a61a1dc49fb66eefc29a1164a1ed8e5f9
vandoris <vandoris@gmail.com>
2025-10-01 16:18:24 +0800

    帳號功能 - 登入

--- src/main/java/com/vandoris/springbootmall/service/UserService.java
+++ src/main/java/com/vandoris/springbootmall/service/UserService.java
@@ -1,11 +1,16 @@
 package com.vandoris.springbootmall.service;
 
+import com.vandoris.springbootmall.dto.UserLoginRequest;
 import com.vandoris.springbootmall.dto.UserRegisterRequest;
 import com.vandoris.springbootmall.model.User;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface UserService {
 
     User getUserById(Integer userId);
 
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 }
